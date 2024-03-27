aa2eab12f02548b1ca6b85c9a78a8b21c90c578f
VIcente MAttos <vicentemattosf@gmail.com>
2024-03-27 14:51:25 -0300

    [FEATURE] Add order details information

---
+++ src/api/get-orders-details.ts
@@ -0,0 +1,30 @@
+import { api } from '@/lib/axios'
+
+interface GetOrderDetailsParams {
+  orderId: string
+}
+
+interface OrderDetailsResponse {
+  id: string
+  createdAt: string
+  status: 'pending' | 'canceled' | 'processing' | 'delivering' | 'delivered'
+  totalInCents: number
+  customer: {
+    name: string
+    email: string
+    phone: string | null
+  }
+  orderItems: {
+    id: string
+    priceInCents: number
+    quantity: number
+    product: {
+      name: string
+    }
+  }[]
+}
+
+export async function getOrderDetails({ orderId }: GetOrderDetailsParams) {
+  const response = await api.get<OrderDetailsResponse>(`/orders/${orderId}`)
+  return response.data
+}
